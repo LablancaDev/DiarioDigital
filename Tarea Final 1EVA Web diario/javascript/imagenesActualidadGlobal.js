5b7b73bcabae1c251854224293843ff49919d6b2
LablancaDev <valenciano_david@hotmail.com>
2023-12-02 11:38:46 +0100

    last commit

--- Tarea Final 1EVA Web diario/javascript/imagenesActualidadGlobal.js
+++ Tarea Final 1EVA Web diario/javascript/imagenesActualidadGlobal.js
@@ -4,44 +4,52 @@ boton.addEventListener("click", mostrarNoticias);
 
 let arrayNoticias = [
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/robot.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/robot.webp",
         texto : "Conoce a ARCHAX, el robot humanoide con brazos funcionales creado en Japón",
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/toyota.jpg",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/toyota.jpg",
         texto : "Toyota suspende temporalmente sus operaciones en Japón"
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/alemania.jpg",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/alemania.jpg",
         texto : "Alemania estudia limitar las cargas de los vehículos eléctricos"
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/biden-xi.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/biden-xi.webp",
         texto : "Biden y Xi se reunirán la próxima semana, ¿qué se espera de este encuentro?" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/suiza.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/suiza.webp",
         texto : "Suiza lucha contra devastadores incendios forestales" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/terremotoUSA.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/terremotoUSA.webp",
         texto : "Fuerte terremoto sacude Texas" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/italia.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/italia.webp",
         texto : "Italia construirá en Albania centros para inmigrantes rescatados en el mar" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/francia.jpg",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/francia.jpg",
         texto : "Macron traslada al presidente de Israel el apoyo de Francia" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/holanda.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/holanda.webp",
         texto : "Mark Rutte consigue la reelección para un cuarto mandato en Países Bajos" 
     },
     {
-        url : "./imágenes/imgs imagenesActualidadGlobal/grecia.webp",
+        url : "./imágenes/imgs-imagenesActualidadGlobal/grecia.webp",
         texto : "Las lluvias torrenciales e inundaciones en Grecia y Turquía" 
+    },
+    {
+        url : "./imágenes/imgs-imagenesActualidadGlobal/tunel.webp",
+        texto : "Así es el túnel sumergido más grande del mundo"
+    },
+    {
+        url : "./imágenes/imgs-imagenesActualidadGlobal/salud.webp",
+        texto : "Inteligencia artificial al servicio de la salud"
     }
 ];
 
@@ -53,10 +61,6 @@ function mostrarNoticias() {
     let imagen = document.getElementById("imagen");
     let texto = document.getElementById("texto");
 
-    // while(indice>0 && indice <=3){
-    //     indice++;
-    // }
-    
     imagen.src = arrayNoticias[indice].url;
     texto.innerText = arrayNoticias[indice].texto;
     
--- Tarea Final 1EVA Web diario/javascript/validarForm.js
+++ Tarea Final 1EVA Web diario/javascript/validarForm.js
@@ -1,24 +1,21 @@
 
-// Agregar un controlador de eventos para el evento "submit" del formulario
+// Validación del Formulario
 
-let formulario = document.getElementById('miFormulario');
+let formulario = document.getElementById('miFormulario');// referencia al formulario
 
-formulario.addEventListener('submit', function (event) {
-    let campos = ['nombre', 'correo', 'edad', 'direccion', 'carta'];
+formulario.addEventListener('submit', function (event) {// se agrega un evento de escucha al formulario para submit, cuando el usuario envía el formulario
+    let campos = ['nombre', 'correo', 'edad', 'direccion', 'carta'];//array que contiene los id´s de cada campo que se valida en el formulario
 
-    campos.forEach(function (campo) {
+    campos.forEach(function (campo) {// se recorren los campos obteniendo cada referencia de los elementos input y span del formulario
         let input = document.getElementById(campo);
         let error = document.getElementById(campo + 'Error');
 
-        if (input.value.trim() === '') {
-            error.textContent = 'Este campo es obligatorio';
-            event.preventDefault(); // Evitar el envío del formulario si hay errores
+        if (input.value.trim() === '') {// se verifica si el campo está vacío con el método trim()
+            error.textContent = 'Este campo es obligatorio';// si está vacío se muestra el mensaje de error
+            event.preventDefault(); // evita el envío del formulario si hay errores
         } else {
-            error.textContent = '';
+            error.textContent = '';// Si el campo no está vacío, se borra cualquier mensaje de error 
         }
     });
 });
 
-
-
-// REPASAR Y ENTENDER PARA DESPÚES EXPLICAR EN LA MEMORIA CAMBIAR LO QUE PUEDA...
